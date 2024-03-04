aa08a0e1b3b452cfca5b0b7236c7097b031152fc
c-southwest <1367248805@qq.com>
2024-03-04 20:41:10 +0100

    dekker: add memory fence

--- user_lock_dekker.cpp
+++ user_lock_dekker.cpp
@@ -2,31 +2,32 @@
 #include <iostream>
 #include <stdio.h>
 
-#define READ_MEMORY_ORDER mo//std::memory_order_relaxed
-#define WRITE_MEMORY_ORDER mo//std::memory_order_relaxed
+#define READ_MEMORY_ORDER std::memory_order_acquire
+#define WRITE_MEMORY_ORDER std::memory_order_release
 
 user_lock_dekker::user_lock_dekker()
     : user_lock() {
     m_flag[0] = m_flag[1] = false;
     m_turn = false;
-    // m_flag_a[0].store(0);
-    // m_flag_a[1].store(0);
-    // m_turn_a.store(0);
+    m_flag_a[0].store(0);
+    m_flag_a[1].store(0);
+    m_turn_a.store(0);
 }
 
-std::memory_order mo = std::memory_order_relaxed;   // strange, use mo program will be correct.
+const std::memory_order mo = std::memory_order_relaxed;   // strange, use mo program will be correct.
 
 void user_lock_dekker::lock(int thread_id) {
     // TODO: Implement the lock acquire part of the Dekker algorithm here
     m_flag_a[thread_id].store(true, WRITE_MEMORY_ORDER);// = true;
+    std::atomic_thread_fence(std::memory_order_seq_cst);
     int j = thread_id == 1 ? 0 : 1;
     while(m_flag_a[j].load(READ_MEMORY_ORDER) == true){
         if(m_turn_a.load(READ_MEMORY_ORDER) != thread_id){
             m_flag_a[thread_id].store(false, WRITE_MEMORY_ORDER);// = false;
             while(m_turn_a.load(READ_MEMORY_ORDER) != thread_id){
-
             }
             m_flag_a[thread_id].store(true, WRITE_MEMORY_ORDER);// = true;
+            std::atomic_thread_fence(std::memory_order_seq_cst);
         }
     }
     // printf("thread %d get the lock, turn:%d, flag[0]=%d, flag[1]=%d\n",
@@ -73,13 +74,4 @@ void user_lock_dekker::unlock(int thread_id) {
     int j = thread_id == 1 ? 0 : 1;
     m_turn_a.store(j, WRITE_MEMORY_ORDER);// = j;
     m_flag_a[thread_id].store(false, WRITE_MEMORY_ORDER);// = false;
-    // printf("thread %d unlock, turn=%d, flag[0]=%d, flag[1]=%d\n", thread_id,
-    // m_turn_a.load(), m_flag_a[0].load(), m_flag_a[1].load());
-
-    // int j = thread_id == 1 ? 0 : 1;
-    // m_turn = j;
-
-    // m_flag[thread_id] = false;
-    // printf("thread %d unlock, turn=%d, flag[0]=%d, flag[1]=%d\n", thread_id,
-    // m_turn, m_flag[0], m_flag[1]);
 }
