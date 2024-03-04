5a2daea2a8c42421661545a4bd75bbeada3ef8d2
c-southwest <1367248805@qq.com>
2024-03-04 10:58:35 +0100

    bonus: memory order task 1

--- atomic_counter_atomic_incdec.cpp
+++ atomic_counter_atomic_incdec.cpp
@@ -1,5 +1,7 @@
 #include "atomic_counters.hpp"
 
+#define MEMORY_ORDER std::memory_order_relaxed
+
 atomic_counter_atomic_incdec::atomic_counter_atomic_incdec()
     : atomic_counter()
     , m_value(0) {
@@ -9,22 +11,22 @@ int atomic_counter_atomic_incdec::increment() {
     // TODO: Modify this code using atomic fetch_add operations
     // int prev_value = m_value;
     // m_value = m_value + 1;
-    return m_value.fetch_add(1);
+    return m_value.fetch_add(1, MEMORY_ORDER);
 }
 
 int atomic_counter_atomic_incdec::decrement() {
     // TODO: Modify this code using atomic fetch_sub operations
     // int prev_value = m_value;
     // m_value = m_value - 1;
-    return m_value.fetch_sub(1);
+    return m_value.fetch_sub(1, MEMORY_ORDER);
 }
 
 void atomic_counter_atomic_incdec::set(int value) {
     // TODO: Modify this code using atomic store operations
-    m_value.store(value);
+    m_value.store(value, MEMORY_ORDER);
 }
 
 int atomic_counter_atomic_incdec::get() {
     // TODO: Modify this code using atomic load operations
-    return m_value.load();
+    return m_value.load(MEMORY_ORDER);
 }
